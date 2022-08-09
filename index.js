997abb7c4ab46fffb9b7ff5e0e5d347742adb715
Hansanca <hansanca@gmail.com>
2022-08-09 09:31:00 -0400

    made port

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ const server = require('./api/server');
 const port = 9000;
 
 // START YOUR SERVER HERE
+
+// console.log('test2')
+
+server.listen(port, () => {
+    console.log('listening on', port)
+})
